023478402ba82fa520ec168dc4973e28dcc5b595
mikeisfake <mikescooper@icloud.com>
2020-03-02 01:24:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+ const newHeader = document.createElement("h1");
+ newHeader.className = "victory"
+ newHeader.id = "victory"
+ newHeader.innerHTML = "Mike is the champion!"
